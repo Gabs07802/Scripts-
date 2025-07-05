a0f5a1f79c084a516cd1a4186eaf6b33aac9b031
Gabs07802 <ctparalol@gmail.com>
2025-07-05 17:31:54 -0300

    Update Aimlock_off.lua

--- Aimlock_off.lua
+++ Aimlock_off.lua
@@ -1,6 +1,3 @@
---# Desativa Aimlock Upper Body Only
-getgenv()._AIMLOCK_UPPER_ONLY = false
-if getgenv()._AIMLOCK_UPPER_CONN then
-    getgenv()._AIMLOCK_UPPER_CONN:Disconnect()
-    getgenv()._AIMLOCK_UPPER_CONN = nil
-end
+-- AIMLOCK UNIVERSAL OFF
+_G.AimlockActive = false
+if _G.AimlockConn then _G.AimlockConn:Disconnect() end
