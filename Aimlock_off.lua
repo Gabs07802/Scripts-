aeb2d0dd52deaa3a739cc3e9168a9c8999318bdb
Gabs07802 <ctparalol@gmail.com>
2025-07-05 01:08:11 -0300

    Update Aimlock_off.lua

--- Aimlock_off.lua
+++ Aimlock_off.lua
@@ -1,4 +1,4 @@
--- Aimlock OFF: Para tudo
+-- Desativa Aimlock corpo inteiro
 _G.AimLockEnabled = false
 if _G.AimLockRenderConn then _G.AimLockRenderConn:Disconnect() end
 _G.AimLockRenderConn = nil
