95575677ed02874b072b7f535548a7b6b671af72
Gabs07802 <ctparalol@gmail.com>
2025-07-06 00:46:55 -0300

    Update STAFF_LIST_OFF.lua

--- STAFF_LIST_OFF.lua
+++ STAFF_LIST_OFF.lua
@@ -1,2 +1,4 @@
-if _G._StaffLine_Con then _G._StaffLine_Con:Disconnect(); _G._StaffLine_Con = nil end
-if _G._StaffLine_Draws then for _,d in pairs(_G._StaffLine_Draws) do pcall(function() d:Remove() end) end _G._StaffLine_Draws = nil end
+-- STAFF LIST OFF: Remove a lista e desconecta atualização
+
+if _G._StaffList_Con then pcall(function() _G._StaffList_Con:Disconnect() end) _G._StaffList_Con = nil end
+if _G._StaffListGui then pcall(function() _G._StaffListGui:Destroy() end) _G._StaffListGui = nil end
